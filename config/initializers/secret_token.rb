46d1b97f7a14a2002bc1000f2ce73e595c72057c
JC <jorge.e.colindres@gmail.com>
2013-12-12 15:00:22 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StoryApp::Application.config.secret_key_base = '19232f98bdfc07ef42e40fc5f010a281a32d9ea11e32830143d2cf4507234de9fae1bdd7bc0a80b642d718d31d3b4ebfe5daa2b3dd6add984a5ee82e691e658d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+StoryApp::Application.config.session_store :cookie_store, key: '_story_app_session'
